90f5ae06d1a7e4af6d4ec3561d9548274f1fafd0
iibrahimli <imranibrahimli98@gmail.com>
2019-11-06 00:05:21 +0400

    add helper functions for data manipulation

---
+++ mlp/data.py
@@ -0,0 +1,21 @@
+"""
+Helper functions to manipulate data
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def normalize(df):
+    """
+    Normalizes a pd.DataFrame
+
+    Args:
+        df:  DataFrame to normalize
+    
+    Returns:
+        res: normalized dataframe
+    """
+
+    pass
