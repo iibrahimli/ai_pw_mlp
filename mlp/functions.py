36ac3fe68934b7eb784912ab16a1eca9b88a27a7
iibrahimli <imranibrahimli98@gmail.com>
2019-11-07 23:34:10 +0400

    add abstrack classes for activation and cost

--- mlp/functions.py
+++ mlp/functions.py
@@ -13,26 +13,47 @@ Todo:
 
 """
 
-
 import numpy as np
 
 
 class activation:
     """
     Abstract class of a differentiable function that
-    contains a forward and a backward pass
+    contains a forward and a backward pass (for backprop)
 
     """
 
     def forward(self, x):
         """
-        Forward pass calculates the output
+        Calculates the output
         """
-        raise NotImplementedError(f"{cls}forward() not implemented")
+        raise NotImplementedError(f"{self.__class__.__name__}.forward() not implemented")
 
 
-    def backward(self, y):
+    def backward(self, y, x_cache):
         """
-        Backward pass calculates the gradient wrt inputs
+        Calculates the gradient wrt inputs
         """
-        raise NotImplementedError(f"backward() not implemented")
+        raise NotImplementedError(f"{self.__class__.__name__}.backward() not implemented")
+
+
+
+class cost:
+    """
+    Abstract class of a differentiable cost that
+    contains a forward and a backward pass (for backprop)
+
+    """
+
+    def forward(self, y_true, y_pred):
+        """
+        Calculates the cost (scalar)
+        """
+        raise NotImplementedError(f"{self.__class__.__name__}.forward() not implemented")
+
+
+    def backward(self, y_true, y_pred):
+        """
+        Calculates the gradient of cost wrt inputs
+        """
+        raise NotImplementedError(f"{self.__class__.__name__}.backward() not implemented")
