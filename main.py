81b32b47ab551de71a7b60fb01a5cf250cc4e761
iibrahimli <imranibrahimli98@gmail.com>
2019-11-10 17:41:46 +0400

    test untrained network

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
-from mlp.network import network
 
+import data
+import mlp.functions as func
+import mlp.network as nn
+
+
+np.random.seed(1337)
 
 # --- read and preprocess data ---
 
@@ -9,12 +14,30 @@ df = pd.read_csv("iris_num.csv", names=['pw', 'pl', 'sw', 'sl', 'label'])
 
 # convert features to numpy array
 feats = df.iloc[:, :4].values.astype(np.float32)
-print("data shape:", feats.shape)
+n_feats = feats.shape[1]
+print(f"features: {feats.shape} {feats.dtype}")
 
-# standardize the data by column
+"""
+Our data is numerical, and the distribution is not centered around 0.
+It would be better to standardize the data to avoid activation function saturation.
+We should remember the mean and standard deviation values to use them on unseen data too.
+"""
+# standardize the data and save parameters
+feats, means, stds = data.feat_standardize(feats)
 
 
 # --- model ---
+net = nn.network([n_feats, 8, 1], [func.tanh(), func.sigmoid()])
+
+# print("weights:")
+# for i, w in net.w.items():
+#     print(f"{i}: {w.shape}")
+
+# print("biases:")
+# for i, b in net.b.items():
+#     print(f"{i}: {b.shape}")
+
+print(net.predict(feats[:10]))
 
 
 # --- train ---
