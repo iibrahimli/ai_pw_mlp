12ef1fd954dd19b5387633939f165b28e2c606ab
iibrahimli <imranibrahimli98@gmail.com>
2019-11-12 10:50:33 +0400

    specify interface

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import data
+import mlp.metrics as metrics
 import mlp.functions as func
 import mlp.network as nn
 
 
 np.random.seed(1337)
 
-# --- read and preprocess data ---
 
 df = pd.read_csv("iris_num.csv", names=['pw', 'pl', 'sw', 'sl', 'label'])
 
 # convert features to numpy array
-feats = df.iloc[:, :4].values.astype(np.float32)
+feats = df.iloc[:, :-1].values.astype(np.float32)
 n_feats = feats.shape[1]
-print(f"features: {feats.shape} {feats.dtype}")
+
+labels = df.iloc[:, -1]
 
 """
 Our data is numerical, and the distribution is not centered around 0.
 It would be better to standardize the data to avoid activation function saturation.
 We should remember the mean and standard deviation values to use them on unseen data too.
 """
-# standardize the data and save parameters
-feats, means, stds = data.feat_standardize(feats)
+X, means, stds = data.feat_standardize(feats)
+
+# one-hot encode labels
+Y = data.int_to_one_hot(labels)
+
+# shuffle the data
+X, Y = data.shuffle(X, Y)
+
+# separate data into training and test sets
+(x_train, y_train), (x_test, y_test) = data.split(X, Y, ratio=0.7)
+
+print("x_train:", x_train.shape, "y_train:", y_train.shape)
+print("x_test:",  x_test.shape,  "y_test:",  y_test.shape)
 
 
-# --- model ---
-net = nn.network([n_feats, 8, 1], [func.tanh(), func.sigmoid()])
-
-# print("weights:")
-# for i, w in net.w.items():
-#     print(f"{i}: {w.shape}")
-
-# print("biases:")
-# for i, b in net.b.items():
-#     print(f"{i}: {b.shape}")
-
-print(net.predict(feats[:10]))
+# network architecture
+net = nn.network([n_feats, 8, 3], [func.tanh(), func.softmax()], func.categorical_crossentropy())
 
 
-# --- train ---
+# training
+history = net.fit(x_train, y_train, lr=0.001, n_epochs=150, batch_size=32, val_ratio=0.2)
+plt.plot(history['loss'], label='loss')
+plt.plot(history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
 
 
-# --- evaluate ---
+# evaluation
+y_test_pred = net.predict(x_test)
+acc = metrics.accuracy()(y_test, y_test_pred)
+print("----------")
+print(f"accuracy on test set ({len(x_test)} samples): {acc:.2f}")
