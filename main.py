1c939d94e4ec2d475c33d20b9dcb24b3eb15eb21
iibrahimli <imranibrahimli98@gmail.com>
2019-11-16 19:25:59 +0400

    fix interface/args

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import data
+import mlp.data as data
 import mlp.metrics as metrics
 import mlp.functions as func
 import mlp.network as nn
 
 
-np.random.seed(1337)
+np.random.seed(123)
 
 
 df = pd.read_csv("iris_num.csv", names=['pw', 'pl', 'sw', 'sl', 'label'])
@@ -33,10 +33,14 @@ Y = data.int_to_one_hot(labels)
 X, Y = data.shuffle(X, Y)
 
 # separate data into training and test sets
-(x_train, y_train), (x_test, y_test) = data.split(X, Y, ratio=0.7)
+(x_train, y_train), (x_test, y_test) = data.split(X, Y, ratio=0.8)
+
+# integer labels
+y_test_int = np.argmax(y_test, axis=1)
 
 print("x_train:", x_train.shape, "y_train:", y_train.shape)
 print("x_test:",  x_test.shape,  "y_test:",  y_test.shape)
+print()
 
 
 # network architecture
@@ -44,15 +48,39 @@ net = nn.network([n_feats, 8, 3], [func.tanh(), func.softmax()], func.categorica
 
 
 # training
-history = net.fit(x_train, y_train, lr=0.001, n_epochs=150, batch_size=32, val_ratio=0.2)
-plt.plot(history['loss'], label='loss')
-plt.plot(history['val_loss'], label='val_loss')
-plt.legend()
-plt.show()
+acc = metrics.accuracy()
+history = net.fit(
+    x_train, y_train,
+    lr=(0.1, 750, 0.5),              # learning rate annealing
+    n_epochs=2500,
+    batch_size=64,
+    val_ratio=0.2,                   # 20% of training data used for validation
+    metrics=[acc],                   # evaluate accuracy for each epoch
+    print_stats=250
+)
 
 
 # evaluation
 y_test_pred = net.predict(x_test)
-acc = metrics.accuracy()(y_test, y_test_pred)
+y_test_pred_int = np.argmax(y_test_pred, axis=1)
+test_acc = acc(y_test_int, y_test_pred_int)
 print("----------")
-print(f"accuracy on test set ({len(x_test)} samples): {acc:.2f}")
+print(f"accuracy on test set ({len(x_test)} samples): {test_acc:.2f}")
+
+
+# plot training
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+# ax1.plot(history['train_loss'], label='train_loss')
+# ax1.plot(history['val_loss'], label='val_loss')
+# ax1.legend()
+# ax1.set_xlabel('epochs')
+# ax1.set_ylabel('loss')
+
+# ax2.plot(history['train_accuracy'], label='train_accuracy')
+# ax2.plot(history['val_accuracy'], label='val_accuracy')
+# ax2.legend()
+# ax2.set_xlabel('epochs')
+# ax2.set_ylabel('accuracy')
+
+# plt.show()
